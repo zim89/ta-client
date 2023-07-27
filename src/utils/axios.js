53b28432072173eae585e8e575e93c574d78fa12
zim89 <121241841+zim89@users.noreply.github.com>
2023-07-27 09:46:37 +0300

    fix2

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://tattooart-e7c517f95819.herokuapp.com',
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://tattooart-e7c517f95819.herokuapp.com',
+  // baseURL: process.env.REACT_APP_API_URL,
 });
 
 // instance.interceptors.request.use(config => {
