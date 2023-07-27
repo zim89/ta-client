03a876cefec3e6207ff7205e1c7eb20303bb5cb6
zim89 <121241841+zim89@users.noreply.github.com>
2023-07-27 09:31:50 +0300

    add variables for versel

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const instance = axios.create({ baseURL: 'http://localhost:4444' });
+const instance = axios.create({
+  // baseURL: 'https://tattooart-e7c517f95819.herokuapp.com',
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
 // instance.interceptors.request.use(config => {
 //   config.headers.Authorization = window.localStorage.getItem('token');
